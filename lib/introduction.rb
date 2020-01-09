66bf32bd84faf2808ba5ccce0232e3996a7e5d5c
Jack Overby <jroverby92@gmail.com>
2020-01-09 02:30:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,5 +12,5 @@ def introduction_with_language(name, language)
 end
   
 def introduction_with_language_optional(name, language = "Ruby")
-  puts introduction_with_language(name, language)
+  introduction_with_language(name, language)
 end
